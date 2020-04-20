b1aaa9337ab53472a069fbedd2f2438e2309ef3e
pak11273 <pak11273@gmail.com>
2020-04-20 00:04:17 -0500

    hotfix: homepage solutions alignment

--- src/components/Text/Text.js
+++ src/components/Text/Text.js
@@ -2,6 +2,7 @@ import styled from "styled-components"
 
 const Text = styled.div`
   color: ${(props) => props.color};
+  display: ${(props) => props.display};
   font-family: ${(props) => props.theme.typography.primaryFont};
   font-size: ${(props) => props.fontsize};
   font-weight: ${(props) => props.fontweight};
@@ -14,7 +15,9 @@ const Text = styled.div`
   width: ${(props) => props.width};
 
   @media (min-width: 768px) {
+    display: ${(props) => props.display768};
     font-size: ${(props) => props.fontsize768};
+    margin: ${(props) => props.margin768};
     text-align: ${(props) => props.textalign768};
     width: ${(props) => props.width768};
   }
